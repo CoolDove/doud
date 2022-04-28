9f6833f05c608e3f760d5c358a12c440a73aee59
Dove <1515098414@qq.com>
2022-04-29 01:10:10 +0800

    hello

--- dcore/src/WindowTk.cpp
+++ dcore/src/WindowTk.cpp
@@ -10,6 +10,16 @@ LRESULT CALLBACK wnd_proc(HWND window, UINT message, WPARAM wparam, LPARAM lpara
     LRESULT result = 0;
     switch (message)
     {
+        case WM_PAINT: {
+            HDC dc_handle;
+            PAINTSTRUCT ps;
+            dc_handle = BeginPaint(window_handle, &ps);
+            RECT rect = {0, 0, 800, 600};
+            SetTextColor(dc_handle, RGB(0, 0, 255));
+            TCHAR* text = _T("Hello Cloud | 你好 云朵");
+            DrawText(dc_handle, text, _tcslen(text), &rect, DT_CENTER | DT_VCENTER);
+            EndPaint(window_handle, &ps);
+        } break;
         case WM_CLOSE: {
             DestroyWindow(window);
         } break;
